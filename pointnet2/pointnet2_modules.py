21e55286e9dd78ca4277b35446ac947e88f9fdc7
Leon Kochiev <kochiev.lg@phystech.edu>
2022-05-23 09:17:11 +0300

    changes for 3DVQA

--- pointnet2/pointnet2_modules.py
+++ pointnet2/pointnet2_modules.py
@@ -19,7 +19,7 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(BASE_DIR)
 
 import pointnet2_utils
-import pytorch_utils_old as pt_utils
+import pytorch_utils as pt_utils
 from typing import List
 
 
